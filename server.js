4ea1e55f8dc29700a7bb702af4ea206d6040a094
Manan Agrawal <agrawalmanan2@gmail.com>
2024-01-20 17:02:30 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+
+const mongodb = "mongodb+srv://agrawalmanan2:12312312@chatbot.fljqvmv.mongodb.net/usermessages"
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.use(express.json())
+
+const messageRouter = require('./routes/messages')
+app.use('/messages', messageRouter)
+
+app.listen(3000, () => console.log('Server Started'))
